7abc6f140e48f4a4f172a99933c394a8e2b8d161
Zer0Tora <101511043+Zer0Tora@users.noreply.github.com>
2024-10-02 20:48:45 +0300

    Create rep

---
+++ src/test/java/org/example/bingsearchtest/MainPageTest.java
@@ -0,0 +1,45 @@
+package org.example.bingsearchtest;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+
+public class MainPageTest {
+    private WebDriver driver;
+
+    @BeforeEach
+    public void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        // Fix the issue https://github.com/SeleniumHQ/selenium/issues/11750
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://www.jetbrains.com/");
+
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void search() {
+        WebElement searchField = driver.findElement(By.cssSelector("[data-test='search-input']"));
+        searchField.sendKeys("Selenium");
+
+        WebElement submitButton = driver.findElement(By.cssSelector("button[data-test='full-search-button']"));
+        submitButton.click();
+
+        WebElement searchPageField = driver.findElement(By.cssSelector("input[data-test='search-input']"));
+        assertEquals("Selenium", searchPageField.getAttribute("value"));
+    }
