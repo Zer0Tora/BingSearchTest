87a77e6881ada285c2adc547b907d7d9223224bc
Zer0Tora <101511043+Zer0Tora@users.noreply.github.com>
2024-10-08 00:24:17 +0300

    Тест выдачи selenium.dev первым элементом

--- src/test/java/org/example/bingsearchtest/MainPageTest.java
+++ src/test/java/org/example/bingsearchtest/MainPageTest.java
@@ -5,12 +5,17 @@ import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainPageTest {
     private WebDriver driver;
@@ -20,11 +25,10 @@ public class MainPageTest {
         ChromeOptions options = new ChromeOptions();
         // Fix the issue https://github.com/SeleniumHQ/selenium/issues/11750
         options.addArguments("--remote-allow-origins=*");
+        options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
         driver = new ChromeDriver(options);
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get("https://www.bing.com/");
-
     }
 
     @AfterEach
@@ -33,14 +37,24 @@ public class MainPageTest {
     }
 
     @Test
-    public void search() {
-        String input = "pancake";
+    public void searchTest() {
+        String input = "selenium";
         WebElement searchField = driver.findElement(By.cssSelector("#sb_form_q"));
         searchField.sendKeys(input);
         searchField.submit();
 
-        WebElement searchPageField = driver.findElement(By.cssSelector("#sb_form_q"));
-        assertEquals(input, searchPageField.getAttribute("value"));
-    }
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(6));
+        wait.until(ExpectedConditions.and(
+                ExpectedConditions.attributeContains(By.cssSelector("h2 > a[href]"), "href", "selenium.dev"),
+                ExpectedConditions.elementToBeClickable(By.cssSelector("h2 > a[href]"))
+        ));
+        List<WebElement> results = driver.findElements(By.cssSelector("h2 > a[href]"));
 
+        results.get(0).click();
+
+        ArrayList<String> tabs = new ArrayList<>(driver.getWindowHandles());
+        driver.switchTo().window(tabs.get(1));
+
+        assertEquals("https://www.selenium.dev/", driver.getCurrentUrl(), "Не совпадает URL");
+    }
 }
