f6ecca25fec95eb79b63560f1d98986b868f4c9e
Zer0Tora <101511043+Zer0Tora@users.noreply.github.com>
2024-10-16 14:04:03 +0300

    Правки кода теста

--- src/test/java/org/example/bingsearchtest/tests/BingSearchTest.java
+++ src/test/java/org/example/bingsearchtest/tests/BingSearchTest.java
@@ -1,17 +1,23 @@
 package org.example.bingsearchtest.tests;
 
-import org.junit.jupiter.api.*;
+import org.example.bingsearchtest.pages.MainPage;
+import org.example.bingsearchtest.pages.ResultPage;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.example.bingsearchtest.pages.MainPage;
-import org.example.bingsearchtest.pages.ResultPage;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BingSearchTest {
     private WebDriver driver;
+    MainPage mp;
+    ResultPage rp;
+    String input = "selenium";
 
     @BeforeEach
     public void setUp() {
@@ -21,6 +27,8 @@ public class BingSearchTest {
         driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         driver.get("http://www.bing.com/");
+        mp = new MainPage(driver);
+        rp = new ResultPage(driver);
     }
 
     @AfterEach
@@ -31,30 +39,17 @@ public class BingSearchTest {
     @Test
     @DisplayName("www.selenium.dev открывается первой ссылкой при поиске через bing.com")
     public void searchResultTest() {
-        String input = "selenium";
-        MainPage mp = new MainPage(driver);
-        ResultPage rp = new ResultPage(driver);
-
         mp.sendText(input);
-
         rp.waitForElement("selenium.dev", 6);
-
         rp.clickElement(0);
-
         rp.goToNewPage();
-
         assertEquals("https://www.selenium.dev/", driver.getCurrentUrl(), "Не совпадает URL");
     }
 
     @Test
     @DisplayName("Введённое значение отображается после поиска")
     public void searchFieldTest() {
-        String input = "selenium";
-        MainPage mp = new MainPage(driver);
-        ResultPage rp = new ResultPage(driver);
-
         mp.sendText(input);
-
         assertEquals(input, rp.checkSearchFieldText(), "Текст в поле не равен: " + input);
     }
 }
